343b3fc7e84cc8d3b8fa1c008f405d628ccced5a
Jono Yan <jono2496@gmail.com>
2025-05-26 00:23:08 +1000

    Update .config/fish/fish_variables Update install-fish.sh

--- run_once_install-fish.sh
+++ run_once_install-fish.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env fish
 
+# Check if fisher function exists
+if functions -q fisher
+    echo "Fisher is installed"
+else
+    echo "Fisher is not installed"
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
