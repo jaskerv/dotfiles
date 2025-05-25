5461c6835811bd9d3e065dc4bc275169ac41f454
Jono Yan <jono2496@gmail.com>
2025-05-25 23:49:10 +1000

    Update run_once_install-packages.sh

--- executable_literal_run_once_install-packages.sh
+++ executable_literal_run_once_install-packages.sh
@@ -5,5 +5,6 @@ echo "Checking for brew install"
 if ! command -v brew &> /dev/null; then
   echo "Brew not install, installing..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install gcc
 fi
 
