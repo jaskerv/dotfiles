5812cec090bc4e53722e6a00eab803e605a5d7a8
Jono Yan <jono2496@gmail.com>
2025-07-15 22:25:38 +1000

    fix

--- run_once_setup_git_set_ssh_chez.sh
+++ run_once_setup_git_set_ssh_chez.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# 1. Get the path to your chezmoi source directory.
-CHEZMOI_SOURCE_DIR=$(chezmoi source-path)
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-# 2. Change into that directory.
-# This cd is part of the script and is not interactive.
+# This will now cause the script to exit if it fails (e.g., due to a lock).
+CHEZMOI_SOURCE_DIR="$HOME/.local/share/chezmoi"
+
+# The rest of the script will only run if the above command was successful.
 cd "$CHEZMOI_SOURCE_DIR"
 
-# 3. Now run your git commands.
 echo "Swapping git remote from HTTPS to SSH..."
 git remote remove origin
 git remote add origin git@github.com:jaskerv/dotfiles.git
