3fbec17b5dd9234193d8cc40f10117b91b507c76
Jono Yan <jono2496@gmail.com>
2025-07-15 22:10:16 +1000

    pull rebase

--- run_once_setup_git.sh
+++ run_once_setup_git.sh
@@ -2,3 +2,4 @@
 git config --global user.email "jono2496@gmail.com"
 git config --global user.name "Jono Yan"
 git config --global --type bool push.autoSetupRemote true
+git config --global pull.rebase true
