b3203c38bd8a1d2e71021a2f4f7ae1fe2f73b895
Jono Yan <jono2496@gmail.com>
2025-07-15 22:17:19 +1000

    fix

--- run_once_setup_git_set_ssh_chez.sh
+++ run_once_setup_git_set_ssh_chez.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-chezmoi cd
+
+# 1. Get the path to your chezmoi source directory.
+CHEZMOI_SOURCE_DIR=$(chezmoi source-path)
+
+# 2. Change into that directory.
+# This cd is part of the script and is not interactive.
+cd "$CHEZMOI_SOURCE_DIR"
+
+# 3. Now run your git commands.
+echo "Swapping git remote from HTTPS to SSH..."
 git remote remove origin
 git remote add origin git@github.com:jaskerv/dotfiles.git
-exit
+
+echo "Done. Current remotes:"
+git remote -v
