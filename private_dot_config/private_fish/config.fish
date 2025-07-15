bea9aca027c3ba3924cd4924bc3a91a8f06025c8
Jono Yan <jono2496@gmail.com>
2025-07-15 22:28:02 +1000

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,17 +4,16 @@ if not functions -q fisher
   curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 end
 
-if not functions -q fisher
-  echo "››› fisher not found. Installing..."
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-end
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
 # disable greetingAdd commentMore actions
 set fish_greeting
+
+# Editor
+set -gx VISUAL "nvim"
+set -gx EDITOR "$VISUAL"
 
 # Starship
 starship init fish | source
