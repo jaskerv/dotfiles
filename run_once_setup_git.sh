93436f8678ff32ff8996f4c1c8337f2aa774429a
Jono Yan <jono2496@gmail.com>
2025-07-21 20:27:06 +1000

    Remove .config/nvim/lazy-lock.json Update setup_git.sh

--- run_once_setup_git.sh
+++ run_once_setup_git.sh
@@ -3,3 +3,4 @@ git config --global user.email "jono2496@gmail.com"
 git config --global user.name "Jono Yan"
 git config --global --type bool push.autoSetupRemote true
 git config --global pull.rebase true
+git config --global init.defaultBranch "main"
